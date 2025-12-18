d74aefa62cce262a15eccfa99c6d75b1ef918853
Gabriel Goffart <gab.goffart@gmail.com>
2025-12-17 22:57:36 -0500

    day08 part 1+2 cleanup

--- day08/main.go
+++ day08/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"slices"
 	"sort"
-	"strconv"
 	"strings"
 
 	"ggof.xyz/aoc2025/lib"
@@ -13,22 +12,22 @@ import (
 
 type p3 struct{ x, y, z int }
 type pdist struct {
-	fromIndex int
-	toIndex   int
-	dist      int
+	p1   p3
+	p2   p3
+	dist int
 }
 
-type ByDistance []pdist
+type byDistance []pdist
 
-func (bd ByDistance) Len() int           { return len(bd) }
-func (bd ByDistance) Swap(i, j int)      { bd[j], bd[i] = bd[i], bd[j] }
-func (bd ByDistance) Less(i, j int) bool { return bd[i].dist < bd[j].dist }
+func (bd byDistance) Len() int           { return len(bd) }
+func (bd byDistance) Swap(i, j int)      { bd[j], bd[i] = bd[i], bd[j] }
+func (bd byDistance) Less(i, j int) bool { return bd[i].dist < bd[j].dist }
 
-type BySize [][]int
+type bySize [][]p3
 
-func (bs BySize) Len() int           { return len(bs) }
-func (bs BySize) Swap(i, j int)      { bs[j], bs[i] = bs[i], bs[j] }
-func (bs BySize) Less(i, j int) bool { return len(bs[i]) > len(bs[j]) }
+func (bs bySize) Len() int           { return len(bs) }
+func (bs bySize) Swap(i, j int)      { bs[j], bs[i] = bs[i], bs[j] }
+func (bs bySize) Less(i, j int) bool { return len(bs[i]) > len(bs[j]) }
 
 func dist(p1, p2 p3) int {
 	dx := p2.x - p1.x
@@ -38,101 +37,67 @@ func dist(p1, p2 p3) int {
 	return (dx * dx) + (dy * dy) + (dz * dz)
 }
 
-func part1(lines []string, take int) int {
+func sortedDistances(lines []string) []pdist {
 	ps := make([]p3, len(lines))
 	for i, l := range lines {
-		parts := strings.Split(l, ",")
-		ps[i].x = lib.Must(strconv.Atoi(parts[0]))
-		ps[i].y = lib.Must(strconv.Atoi(parts[1]))
-		ps[i].z = lib.Must(strconv.Atoi(parts[2]))
+		fmt.Sscanf(l, "%d,%d,%d", &ps[i].x, &ps[i].y, &ps[i].z)
 	}
 
-	dists := make([]pdist, 0)
+	dists := make([]pdist, 0, len(ps)*len(ps))
 	for i, p1 := range ps {
 		for j := i + 1; j < len(ps); j++ {
 			p2 := ps[j]
-			dists = append(dists, pdist{i, j, dist(p1, p2)})
+			dists = append(dists, pdist{p1, p2, dist(p1, p2)})
 		}
 	}
 
-	sort.Sort(ByDistance(dists))
-	dists = dists[:take]
+	sort.Sort(byDistance(dists))
+	return dists
+}
 
+func updateCircuits(circuits [][]p3, d pdist) [][]p3 {
+	p1i := slices.IndexFunc(circuits, func(c []p3) bool { return slices.Contains(c, d.p1) })
+	p2i := slices.IndexFunc(circuits, func(c []p3) bool { return slices.Contains(c, d.p2) })
+
+	switch {
+	case p1i != -1 && p2i != -1 && p1i != p2i:
+		circuits[p1i] = append(circuits[p1i], circuits[p2i]...)
+		circuits[p2i] = circuits[0]
+		circuits = circuits[1:]
+	case p1i == -1 && p2i != -1:
+		circuits[p2i] = append(circuits[p2i], d.p1)
+	case p1i != -1 && p2i == -1:
+		circuits[p1i] = append(circuits[p1i], d.p2)
+	case p1i == -1 && p2i == -1:
+		circuits = append(circuits, []p3{d.p1, d.p2})
+	}
+
+	return circuits
+}
+
+func part1(lines []string, take int) int {
+	dists := sortedDistances(lines)
 	// make an array of the indexes of the points
-	circuits := make([][]int, 0)
-	for _, d := range dists {
-		circuitForFromIndex := slices.IndexFunc(circuits, func(c []int) bool { return slices.Contains(c, d.fromIndex) })
-		circuitForToIndex := slices.IndexFunc(circuits, func(c []int) bool { return slices.Contains(c, d.toIndex) })
-
-		switch {
-		case circuitForFromIndex != -1 && circuitForToIndex != -1 && circuitForFromIndex != circuitForToIndex:
-			// concat them
-			circuits[circuitForFromIndex] = append(circuits[circuitForFromIndex], circuits[circuitForToIndex]...)
-
-			// swap with last + remove
-			circuits[circuitForToIndex] = circuits[0]
-			circuits = circuits[1:]
-		case circuitForFromIndex == -1 && circuitForToIndex != -1:
-			circuits[circuitForToIndex] = append(circuits[circuitForToIndex], d.fromIndex)
-		case circuitForFromIndex != -1 && circuitForToIndex == -1:
-			circuits[circuitForFromIndex] = append(circuits[circuitForFromIndex], d.toIndex)
-		case circuitForFromIndex == -1 && circuitForToIndex == -1:
-			circuits = append(circuits, []int{d.fromIndex, d.toIndex})
-		}
+	var circuits [][]p3
+	for _, d := range dists[:take] {
+		circuits = updateCircuits(circuits, d)
 	}
 
-	sort.Sort(BySize(circuits))
+	sort.Sort(bySize(circuits))
 
 	return len(circuits[0]) * len(circuits[1]) * len(circuits[2])
 }
 
 func part2(lines []string) int {
-
-	ps := make([]p3, len(lines))
-	for i, l := range lines {
-		parts := strings.Split(l, ",")
-		ps[i].x = lib.Must(strconv.Atoi(parts[0]))
-		ps[i].y = lib.Must(strconv.Atoi(parts[1]))
-		ps[i].z = lib.Must(strconv.Atoi(parts[2]))
-	}
-
-	dists := make([]pdist, 0)
-	for i, p1 := range ps {
-		for j := i + 1; j < len(ps); j++ {
-			p2 := ps[j]
-			dists = append(dists, pdist{i, j, dist(p1, p2)})
-		}
-	}
-
-	sort.Sort(ByDistance(dists))
-
-	// make an array of the indexes of the points
-	circuits := make([][]int, 0)
+	dists := sortedDistances(lines)
+	var circuits [][]p3
 	for _, d := range dists {
-		circuitForFromIndex := slices.IndexFunc(circuits, func(c []int) bool { return slices.Contains(c, d.fromIndex) })
-		circuitForToIndex := slices.IndexFunc(circuits, func(c []int) bool { return slices.Contains(c, d.toIndex) })
+		circuits = updateCircuits(circuits, d)
 
-		switch {
-		case circuitForFromIndex != -1 && circuitForToIndex != -1 && circuitForFromIndex != circuitForToIndex:
-			// concat them
-			circuits[circuitForFromIndex] = append(circuits[circuitForFromIndex], circuits[circuitForToIndex]...)
-
-			// swap with last + remove
-			circuits[circuitForToIndex] = circuits[0]
-			circuits = circuits[1:]
-		case circuitForFromIndex == -1 && circuitForToIndex != -1:
-			circuits[circuitForToIndex] = append(circuits[circuitForToIndex], d.fromIndex)
-		case circuitForFromIndex != -1 && circuitForToIndex == -1:
-			circuits[circuitForFromIndex] = append(circuits[circuitForFromIndex], d.toIndex)
-		case circuitForFromIndex == -1 && circuitForToIndex == -1:
-			circuits = append(circuits, []int{d.fromIndex, d.toIndex})
-		}
-
-		if len(circuits) == 1 && len(circuits[0])	== len(lines) {
-			return ps[d.fromIndex].x * ps[d.toIndex].x
+		if len(circuits) == 1 && len(circuits[0]) == len(lines) {
+			return d.p1.x * d.p2.x
 		}
 	}
-
 
 	panic("arrived at the end without connecting all boxes")
 }
